deaaa7785a13ee454adf8c33838831c1bf8e54cb
виталик рыбаков <vita250r@yandex.ru>
2018-03-18 11:58:53 +0300

    через ini файл

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 """
 Created on Sat Mar 17 16:37:07 2018
 
 @author: 123
 """
 import telebot
-bot = telebot.TeleBot('481180883:AAFjGPMJVPKL0xaH2MksG4GlH5bprwwxdpI')
+import config
+bot = telebot.TeleBot(config.token)
 
 
 @bot.message_handler(commands = ['start'])
