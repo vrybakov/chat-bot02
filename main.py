32d7c1934dae62e623738049433d27ccda35da50
виталик рыбаков <vita250r@yandex.ru>
2018-03-21 19:48:54 +0300

    now

--- main.py
+++ main.py
@@ -88,29 +88,29 @@ def test(message):
 
     
 
-# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://calm-ravine-51322.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
+## Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+#if "HEROKU" in list(os.environ.keys()):
+#    logger = telebot.logger
+#    telebot.logger.setLevel(logging.INFO)
+#
+#    server = Flask(__name__)
+#    @server.route("/bot", methods=['POST'])
+#    def getMessage():
+#        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#        return "!", 200
+#    @server.route("/")
+#    def webhook():
+#        bot.remove_webhook()
+#        bot.set_webhook(url="https://calm-ravine-51322.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
+#        return "?", 200
+#    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+#else:
+#    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
+#    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+#    bot.remove_webhook()
+#    bot.polling(none_stop=True)
 
 #
 #
-#if __name__ == '__main__':
-#    bot.polling(none_stop = True) 
+if __name__ == '__main__':
+    bot.polling(none_stop = True) 
